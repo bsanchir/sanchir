f114d0ecbff031d329307f2feb0d6240ed1e19af
Luvsandagva Bayanmunkh <2003sassas@gmail.com>
2025-04-27 15:19:11 +0800

    cvbn

---
+++ lab8_1.js
@@ -0,0 +1,14 @@
+var n ;
+n = window.prompt("hoorondiin zai meter : " ) ;
+var wolf = 6.94 ;
+var rabbit = 5 ;
+var sw ; 
+var sr ;
+var time ;
+sw=n;
+while (sw == sr) {
+    sw=sw+wolf ;
+    sr=sr+rabbit ;
+    time++ ;
+}
+document.write("wolf will capture rabit in :" , sum) ;
---
+++ lab8_3.js
@@ -0,0 +1,10 @@
+var n;
+n = window.prompt("too oruul :");
+const a=[] ;
+var i ;
+for ( i=0 ; i<5 ; i++  ) {
+    a[i]= window.prompt("array utga av :");
+}
+for ( i = 0 ; i < 5 ; i++ ) {
+    
+}
